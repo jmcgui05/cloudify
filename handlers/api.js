73d6db97c7c28879b926b417aa43301d316bfdf3
jmcgui05 <johnmac81@gmail.com>
2021-05-02 18:31:36 -0500

    POST complete

--- handlers/api.js
+++ handlers/api.js
@@ -1,12 +1,59 @@
-async function hello(event) {
-  console.log(JSON.stringify(event, undefined, 2));
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/html' },
-    body: "Hello from lambda"
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB;
+const TABLE_NAME = process.env.TABLE_NAME || "";
+const PRIMARY_KEY = process.env.PRIMARY_KEY || "";
+
+async function postProduct(event) {
+  const { productId, name, price, tags } = event;
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      "productId": {
+        S: productId
+      },
+      "name": {
+        S: name
+      },
+      "price": {
+        N: price
+      },
+      "tags": {
+        SS: tags
+      } 
+    }
+  };
+
+  try {
+    const response = await db 
+      .putItem(params)
+      .promise()
+      .then(res => res)
+      .catch(err => err);
+
+    return {
+      statusCode: 200,
+      body: `Successfully saved product with id ${productId}`
+    }
+
+  } catch (err) {
+    return {
+      statusCode: err.statusCode,
+      body: err.message
+    }
   }
 }
 
+async function getProductById(event) {
+
+}
+
+async function getProductByTags(event) {
+
+}
+
 module.exports = {
-  hello
+  postProduct,
+  getProductById,
+  getProductByTags
 }
